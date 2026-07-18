4212ac8a6bae8f1c6a41b8ae69a12ea14a422cbf
87edik@gmail.com <87edikgmail.com@MacBook-Pro-87edikgmailcom.local>
2026-07-18 21:20:13 +0300

    First homework done

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,59 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    private int size = 0;
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i] != null && uuid.equals(storage[i].uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        int index = -1;
+        for (int i = 0; i < size; i++) {
+            if (storage[i] != null && uuid.equals(storage[i].uuid)) {
+                index = i;
+                break;
+            }
+        }
+        if (index >= 0) {
+            for (int i = index; i < size - 1; i++) {
+                storage[i] = storage[i + 1];
+            }
+            storage[size - 1] = null;
+            size--;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
